2e3e44e8a0ba7709c2f849efbb796d80fde27ef6
Michael Evans <michaelevans@Michaels-MacBook-Pro.local>
2021-08-20 15:46:53 -0600

    working on debugging store duplicate issue

--- app/models/sound-match/sound-match-store.ts
+++ app/models/sound-match/sound-match-store.ts
@@ -3,43 +3,58 @@ import { getSnapshot, Instance, SnapshotOut, types } from "mobx-state-tree"
 // import { CharacterApi } from "../../services/api/character-api"
 import { withEnvironment } from "../extensions/with-environment"
 
-import { SoundMatchGameModel, SoundMatchPairModel, SoundMatchGameSnapshot, SoundMatchPairSnapshot, SoundMatchPair } from "./sound-match-game"
+import {
+  SoundMatchGameModel,
+  SoundMatchPairModel,
+  SoundMatchGameSnapshot,
+  SoundMatchPairSnapshot,
+  SoundMatchPair,
+} from "./sound-match-game"
 /**
  * Example store containing Rick and Morty characters
  */
 
-export type SoundMatchPairs = {questionText: string, answerText: string}[]
-
-let idx = 1
+export type SoundMatchPairs = { questionText: string; answerText: string }[]
 
 export const SoundMatchStoreModel = types
   .model("SoundMatchStore")
   .props({
+    id: types.optional(types.number, 1),
     games: types.optional(types.array(SoundMatchGameModel), []),
     pairs: types.optional(types.array(SoundMatchPairModel), []),
   })
   .extend(withEnvironment)
   .actions((self) => ({
+    nextId: () => {
+      const { id } = self
+      self.id = self.id + 1
+      return id
+    },
     // saveCharacters: (snapshots: SoundMatchGameSnapshot[]) => {
     //   self.games.replace(snapshots)
     // },
   }))
   .actions((self) => ({
-    createGame: (title: string, pairings: SoundMatchPairs ) => {
-      console.log('create the game', title, pairings)
-      const pairs = pairings.map(p => SoundMatchPairModel.create({ id: idx++, ...p }))
-      self.pairs.replace( self.pairs.concat(self.pairs, pairs) )
-      const game = SoundMatchGameModel.create({ id: idx++, title, pairs: pairs.map(p => p.id) })
+    createGame: (title: string, pairings: SoundMatchPairs) => {
+      console.log("create the game", title, pairings)
+      const pairs = pairings.map((p) => SoundMatchPairModel.create({ id: self.nextId(), ...p }))
+      console.log(self.nextId)
+      self.pairs.replace(self.pairs.concat(self.pairs, pairs))
+      const game = SoundMatchGameModel.create({
+        id: self.nextId(),
+        title,
+        pairs: pairs.map((p) => p.id),
+      })
       self.games.push(game)
     },
     getGame: (id: number) => {
-      const game = self.games.find(e => e.id === id)
+      const game = self.games.find((e) => e.id === id)
       return game
     },
     deleteEverything: () => {
       self.games.replace([])
       self.pairs.replace([])
-    }
+    },
     // getCharacters: async () => {
     //   const characterApi = new CharacterApi(self.environment.api)
     //   const result = await characterApi.getCharacters()
